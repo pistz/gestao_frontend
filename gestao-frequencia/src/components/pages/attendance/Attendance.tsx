a7b0955466bbeecacc89f1dfeae73b76b3dfdea3
fgmarcal <felipegarciamarcal@gmail.com>
2024-04-08 21:57:30 -0300

    create attendanceList layout

--- gestao-frequencia/src/components/pages/attendance/Attendance.tsx
+++ gestao-frequencia/src/components/pages/attendance/Attendance.tsx
@@ -1,21 +1,125 @@
-import { Divider } from 'antd'
-import React from 'react'
+import { Divider, Drawer, Select } from 'antd'
+import React, { useEffect, useState } from 'react'
 import { listContainerDivStyle } from '../lists/styles';
-import { attendanceHeaderStyle } from './styles';
+import { attendanceHeaderStyle, attendanceMainStyle } from './styles';
+import { CourseRepository } from '../../../repository/CourseRepository';
+import { AttendanceList } from '../../../repository/ListRepository';
+import { useTableData } from '../../../hooks/useTableData';
+import { Button } from '../../shared/button/Button';
+
+const courseData = new CourseRepository(); 
+const listData = new AttendanceList();
 
 export const Attendance:React.FC = () => {
 
+    const [courseOptions, setCourseOptions] = useState<{value:string, label:string}[]>([]);
+    const [selectedCourse, setSelectedCourse] = useState<string | undefined>(undefined);
+    const {attendanceTableData, setAttendanceTableData, listsTableData, setListsTableData} = useTableData();
+    const [open, setOpen] = useState(false);
+    const [date, setDate] = useState<string>('')
+
+    const showDrawer = () => {
+      setOpen(true);
+    };
+  
+    const onClose = () => {
+      setOpen(false);
+    };
+
     const dividerText = (text:string):string => {
         return text.toUpperCase();
-    }
+    };
 
+    useEffect(() =>{
+        const fetchCourseData = async () =>{
+            try {
+                const courses = await courseData.getAllCourses();
+                const options = courses.map(course => ({
+                    value:course.id,
+                    label:course.name
+                }));
+                setCourseOptions(options);
+            } catch (error) {
+                console.error('Error fetching courses:', error);
+            }
+        };
+        fetchCourseData();
+    },[]);
+
+    const handleCourseChange = async (value: string) => {
+        setSelectedCourse(value);
+        const lists = listData.getAllAttendanceLists();
+        const filteredList = (await lists).filter((list) => list.courseId === value);
+        setListsTableData(filteredList)
+    };
+
+    const isValidDate = (dateString: string): boolean => {
+        const dateFormat = /^\d{2}\/\d{2}\/\d{4}$/;
+    
+        if (!dateString.match(dateFormat)) {
+            return false;
+        }
+    
+        const parts = dateString.split("/");
+        const day = parseInt(parts[0], 10);
+        const month = parseInt(parts[1], 10) - 1;
+        const year = parseInt(parts[2], 10);
+    
+        const inputDate = new Date(year, month, day);
+    
+        if (
+            isNaN(inputDate.getTime()) ||
+            inputDate.getDate() !== day ||
+            inputDate.getMonth() !== month ||
+            inputDate.getFullYear() !== year
+        ) {
+            return false;
+        }
+    
+        const currentDate = new Date();
+        return inputDate <= currentDate
+
+    };
+    
     return (
         <>
         <div style={listContainerDivStyle}>
                 <header style={attendanceHeaderStyle}>
                 <Divider>{dividerText('chamada')}</Divider>
                 </header>
+                <section style={attendanceMainStyle}>
+                    <p style={{marginBottom:'0.5rem'}}>Matéria</p>
+                    <Select
+                        showSearch
+                        style={{ width: '15rem' }}
+                        placeholder="Selecione a matéria"
+                        optionFilterProp="children"
+                        filterOption={(input, option) => (option?.label ?? '').includes(input)}
+                        filterSort={(optionA, optionB) =>
+                        (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
+                        }
+                        options={courseOptions}
+                        onChange={handleCourseChange}
+                    />
+                    
+                </section>
         </div>
+        <Divider>{dividerText('chamadas disponíveis')}</Divider>
+        <div style={attendanceMainStyle}>
+            {selectedCourse && listsTableData.map((list, index) => {
+                if (isValidDate(list.attendanceDate)) {
+                    return (
+                        <Button key={index} text={list.attendanceDate} type='button' click={showDrawer} />
+                    );
+                }
+                return null;
+            })}
+        </div>
+        <Drawer title={date} onClose={onClose} open={open} size='large'>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+        </Drawer>
 
         
         </>
